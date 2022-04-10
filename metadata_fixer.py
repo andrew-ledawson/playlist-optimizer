a039079358dc2f5d93e829987592be2e2b3139ac
Andrew LeDawson <offers@andrewiam.com>
2022-04-10 02:36:48 -0700

    Work checkin

---
+++ metadata_fixer.py
@@ -0,0 +1,7 @@
+from foundation import *
+
+# TODO: move playlist chooser out of rater, into foundation and prompt user for songs to fix
+# TODO: prompt user if they want to review flagged songs or do thorough review of all songs
+# TODO: Call info func
+# TODO: If thorough, check video ID against YouTube and prompt if private
+# TODO: if thorough and private fixes made, prompt user to replace remote playlist
--- scratchpad.py
+++ scratchpad.py
@@ -1,8 +1,15 @@
 from foundation import *
 
-"""saved_playlists, all_songs = load_data_files('.')
+saved_playlists, all_songs = load_data_files('.')
+ids_to_remove = []
+for id, song in all_songs.items():
+    if type(song) is not Song:
+        ids_to_remove.append(id)
+        print("Popping " + id)
+for id in ids_to_remove:
+    all_songs.pop(id)
 
-for index, song_a in enumerate(list(all_songs.values())):
+"""for index, song_a in enumerate(list(all_songs.values())):
     #song_a.user_ratings = dict()
     for song_b_index in range(index+1, len(all_songs.values())):
         song_b = list(all_songs.values())[song_b_index]
@@ -10,10 +17,11 @@ for index, song_a in enumerate(list(all_songs.values())):
         if song_a.user_ratings is song_b.user_ratings:
             print("Shared ratings object detected between " + song_a.name + " and " + song_b.name)"""
 
-#write_song_db(all_songs)
+write_song_db(all_songs)
 
-
+"""
 song_id = "DwanG6dAl18"
 metadata = download_metadata_from_YT_id(song_id)
 #response = YTM.get_watch_playlist(song_id)
 print("done")
+"""
