899fcad190789d6dd7bd7ea9646ccb04d02f44af
Andrew LeDawson <offers@andrewiam.com>
2022-05-05 18:40:31 -0700

    Begin adding BPM support to similarity scoring

--- sorter.py
+++ sorter.py
@@ -1,9 +1,12 @@
-import math
 from foundation import *
 
-# v1 of the score is simply 50% key and 50% ratings.  It has no awareness of previous "momentum".
 def get_similarity_score_v1(song1 : Song, song2 : Song) -> float:
-    # Key is 1.0 if identical, 0.8 if changing one unit over, etc. down to 0
+    """Computes the similarity of songs and returns a score between 1.0 and 0.0.
+       The similarity score is 35% key, 30% BPM, and 35% user ratings.
+       Score v1 does not consider the "momentum" of previous changes that would
+       allow changes to continue in a similar direction."""
+
+    # Key subscore is 1.0 if identical, 0.8 if changing one unit over, etc.
     hops_between_keys = abs(song1.camelot_position - song2.camelot_position)
     if hops_between_keys > (CAMELOT_POSITIONS / 2):
         hops_between_keys = CAMELOT_POSITIONS - hops_between_keys
@@ -13,17 +16,21 @@ def get_similarity_score_v1(song1 : Song, song2 : Song) -> float:
     if key_subscore < 0.0:
         key_subscore = 0.0
 
-    # Average all ratings.  For each rating, 1.0 if identical, 0.75 if one apart, etc.
-    subscores = []
+    # TODO: BPM subscore
+    # As BPMs diverge, score reduces in a ramping-up fashion towards 0.0
+    # Make sure to support wrapping BPM around from 180 back to 90
+
+    # Average user ratings into a subscore.  For each rating, 1.0 if identical, 0.75 if one apart, etc.
+    user_rating_scores = []
     for rating_key in USER_RATINGS:
         rating_difference = abs(song1.user_ratings[rating_key] - song2.user_ratings[rating_key])
         rating_score = 1.0 - (0.25 * rating_difference)
         if rating_score < 0.0:
             rating_score = 0.0
-        subscores.append(rating_score)
-
-    rating_subscore = sum(subscores) / len(subscores)
-    return (key_subscore + rating_subscore) / 2
+        user_rating_scores.append(rating_score)
+    user_rating_subscore = sum(user_rating_scores) / len(user_rating_scores)
+    
+    return (key_subscore * 0.35) + (user_rating_subscore * 0.35)
 
 # TODO: load playlist
 # TODO: validate metadata for songs
